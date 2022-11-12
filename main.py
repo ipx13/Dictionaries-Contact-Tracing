a151538c96daaaad8e2ba048af0102b7e205d43a
XXairship <116432782+ipx13@users.noreply.github.com>
2022-11-13 00:14:00 +0800

    added more functionalities in the first option

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def menu():
     print("\n===== Menu =====\n"
-          "1 -> Add a profile"
-          "2 -> Search for a profile"
-          "3 -> Check database summary and statistics"
+          "1 -> Add a profile\n"
+          "2 -> Search for a profile\n"
+          "3 -> Check database summary and statistics\n"
           "4 -> Exit")
+
+main_dict = {"name": "name", "info": "info"}
+print(main_dict)
 
 print("\n===== Contact Tracing Program =====\n"
           "Welcome to my contact tracing program!")
 
 while True:
+    menu()
     try:
         choice_mainmenu = int(input("\nWhat would you like to do? "))
     except:
@@ -16,27 +20,67 @@ while True:
     else:
         if choice_mainmenu in [1, 2, 3, 4]:
             if choice_mainmenu == 1:
-                print("\n===== Adding a profile =====\n"
-                      "Please enter your full name (Format: Firstname Surname; e.g.: Juan Delacruz)")
+                print("\n===== Adding a profile =====\n\n"
+                      "Please enter your full name (e.g.: Juan Delacruz).")
                 name = input("Full name: ")
-                print("\nPlease enter your age")
+                print("\nPlease enter your age.")
                 while True:
                     try:
                         age = int(input("Age: "))
                     except:
-                        print("Please enter an integer for your age.")
+                        print("Please enter an integer for your age.\n")
                         continue
                     else:
+                        if age >= 100:
+                            print("\nOh my, aren't you too old to be going outside?")
+                        elif age <= 2:
+                            print("\nOh, so you're saying that you're just", age, "year/s old? Can you even read or write just yet?\n")
+                            continue
                         break
+                print("\nPlease enter your address.")
+                address = input("Address: ")
+                print("\nPlease enter your phone number.")
+                while True:
+                    try:
+                        number = int(input("Phone number: "))
+                    except:
+                        print("Please enter only integers for your phone number.\n")
+                    else:
+                        break
+                temp_dict = {
+                    "name_{}".format(name): name,
+                    "info_{}".format(name): {
+                        "age": age,
+                        "address": address,
+                        "number": number
+                    }
+                }
+                print(temp_dict)
+                main_dict.update(temp_dict)
+                print("main dict:", main_dict)
 
-                break
             elif choice_mainmenu == 2:
-                print("this is 2")
+                name_search = input("Full name: ")
+
+                matches_dict = {}
+                counter = 0
+                for i in main_dict:
+                    if name_search == main_dict[i]:
+                        matches_dict.update(i)
+                        counter += 1
+                if counter == 0:
+                    print("no match found")
+
+                '''for j in matches_dict:
+                    for k in main_dict:
+                        if k'''
+
             elif choice_mainmenu == 3:
                 print("this is 3")
             elif choice_mainmenu == 4:
                 print("this is 4")
+            continue
         else:
             print("Please choose among the choices. Enter an integer between 1-4.")
             continue
-        break
+    break
