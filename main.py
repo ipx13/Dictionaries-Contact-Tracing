2a840a21a354ebe0a82f03d1cbe74cace4fd7ec6
XXairship <116432782+ipx13@users.noreply.github.com>
2022-11-12 20:22:28 +0800

    Added a menu function and a greeting sorta thing

---
+++ main.py
@@ -0,0 +1,9 @@
+def menu():
+    print("\n===== Menu =====\n"
+          "1 -> Add a profile"
+          "2 -> Search for a profile"
+          "3 -> Check database summary and statistics"
+          "4 -> Exit")
+
+print("\n===== Contact Tracing Program =====\n"
+          "Welcome to my contact tracing program!")
