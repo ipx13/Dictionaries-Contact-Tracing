09bc812cc3cfc51021c5d52aedf1780c0e5a323a
XXairship <116432782+ipx13@users.noreply.github.com>
2022-11-12 20:32:38 +0800

    Added the input prompts and if-else statement for the menu

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ def menu():
 
 print("\n===== Contact Tracing Program =====\n"
           "Welcome to my contact tracing program!")
+
+while True:
+    try:
+        choice_mainmenu = int(input("What would you like to do?"))
+    except:
+        print("Please enter only an integer! Choose between choices 1-4.")
+    else:
+        if choice_mainmenu in [1, 2, 3, 4]:
+            if choice_mainmenu == 1:
+                print("gottem")
+                break
+        else:
+            print("Please choose among the choices. Enter an integer between 1-4.")
+            continue
+        break
